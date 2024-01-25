7d5abb7607f14967ea3efa59b8b71f030d66da61
Jellyfish1223 <69008277+Jellyfish1223@users.noreply.github.com>
2024-01-25 22:55:25 +0000

    Booking and Menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True, validators=[MaxValueValidator(99999999999)])
+    name = models.CharField(max_length=255)
+    guests = models.PositiveSmallIntegerField(validators=[MaxValueValidator(6)])
+    date = models.DateTimeField()
+
+class Menu(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, validators=[MaxValueValidator(99999)])
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(validators=[MaxValueValidator(5)])
+
+    
